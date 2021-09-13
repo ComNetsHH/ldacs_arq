c0006d3e53bdead8e821df64f54f83164b5f3072
Konrad Fuger <k.fuger@tuhh.de>
2021-09-13 12:45:23 +0200

    fix handleAck Test

--- dev/tests/SelectiveRepeatArqProcessTest.cpp
+++ dev/tests/SelectiveRepeatArqProcessTest.cpp
@@ -213,11 +213,13 @@ public:
         auto reply = make_pair(&replyHeader, &payload);
         process.processLowerLayerSegment(reply);
 
-        CPPUNIT_ASSERT(process.hasRtxSegment(100));
+        CPPUNIT_ASSERT(process.hasRtxSegment(1000));
 
 
-        L2Packet *rtxSegment = process.getRtxSegment(100);
+        L2Packet *rtxSegment = process.getRtxSegment(1000);
         auto unicastHeaders = PacketUtils::getUnicastFragments(rtxSegment);
+
+        cout << rtxSegment->print() << endl;
 
         CPPUNIT_ASSERT_EQUAL(1, (int)unicastHeaders.size());
         L2HeaderUnicast *rtxHeader = (L2HeaderUnicast*) unicastHeaders[0].first;
